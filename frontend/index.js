c8ef85f0602704cc145722e0be44db6199a28449
inż. Krystian Flak <flak99@o2.pl>
2025-01-05 03:10:53 +0100

    Dodanie listy dat

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,34 @@
 // const zadania = ["Umyc rece", "kupic pączka", "Sprzedać nerkę", "poczytać"];
 
-let przycisk = document.getElementById("btn");
-let inputArea = document.getElementById("inputArea");
-let zadania = document.querySelectorAll(".ala");
+// let przycisk = document.getElementById("btn");
+// let inputArea = document.getElementById("inputArea");
+// let zadania = document.querySelectorAll(".ala");
 
-przycisk.addEventListener("click", function () {
-  inputArea.classList.toggle("open");
-  zadania.forEach((e) => {
-    e.classList.add("open");
-  });
-  console.log("click");
+// przycisk.addEventListener("click", function () {
+//   inputArea.classList.toggle("open");
+//   zadania.forEach((e) => {
+//     e.classList.add("open");
+//   });
+//   console.log("click");
+// });
+
+// Wyświetlanie dat
+const dataArray = [];
+const dataAreaView = document.getElementById("dates_container_ID");
+
+for (let i = 1; i < 10; i++) {
+  let data = `0${i}.01`;
+  dataArray.push(data);
+}
+for (let i = 10; i <= 31; i++) {
+  let data = `${i}.01`;
+  dataArray.push(data);
+}
+console.log(dataArray);
+
+dataArray.forEach((data) => {
+  let dataSet = document.createElement("div");
+  dataSet.classList.add("scrol-items");
+  dataSet.innerHTML = data;
+  dataAreaView.appendChild(dataSet);
 });
