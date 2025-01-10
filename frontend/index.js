6d004423f4019bdae4fab75015e5e9ee60102c3b
inż. Krystian Flak <flak99@o2.pl>
2025-01-11 00:56:52 +0100

    Dodanie systemu wystelania dat (poprawny sposób) dodanie wysttlania daty oraz ilosci zadan na footer

--- frontend/index.js
+++ frontend/index.js
@@ -2,24 +2,43 @@
 const dataArray = [];
 const dataAreaView = document.getElementById("dates_container_ID");
 
-for (let i = 1; i < 10; i++) {
-  let data = `0${i}.01`;
-  dataArray.push(data);
-}
-for (let i = 10; i <= 31; i++) {
-  let data = `${i}.01`;
-  dataArray.push(data);
+function generateDates() {
+  const year = new Date().getFullYear(); // Pobierz bieżący rok
+  let currentDate = new Date(year, 0, 1); // Ustaw datę na 1 stycznia
+  const endDate = new Date(year, 0, 31); // Ustaw datę na 31 Stycznia
+
+  while (currentDate <= endDate) {
+    // Formatowanie daty do DD.MM
+    const day = String(currentDate.getDate()).padStart(2, "0");
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    dataArray.push(`${day}.${month}`);
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
 }
 
-dataArray.forEach((data) => {
-  let dataSet = document.createElement("div");
-  dataSet.classList.add("scrol-items");
-  dataSet.innerHTML = data;
-  dataAreaView.appendChild(dataSet);
-});
+function displayDates() {
+  dataArray.forEach((data) => {
+    let dataSet = document.createElement("div");
+    dataSet.classList.add("scrol-items");
+    dataSet.innerHTML = data;
+    dataAreaView.appendChild(dataSet);
+  });
+}
+
+generateDates();
+displayDates();
+
+// Footer wystwlanie dziajeszej daty
+let footerMonth = new Date().getMonth();
+let footerDay = new Date().getDate();
+const months = ["STY", "LUT", "MAR"];
+let dateLabelFooter = document.getElementById("data_label_ID");
+dateLabelFooter.textContent = footerDay + " " + months[footerMonth];
+
+let taskCountArea = document.getElementById("number_of_task_ID");
+taskCountArea.textContent = `0 zadań`;
 
 // Tworzenie Zadań jako obiekty
-
 class TaskClass {
   constructor(taskText, taskData) {
     this.text = taskText;
@@ -81,4 +100,9 @@ function ShowTask() {
       ShowTask(); // Odśwież listę
     });
   });
+
+  // Footer wystwtlanie ilosciz dan na dziś
+  let taskCount = listOfTaskCont.length;
+  let taskCountArea = document.getElementById("number_of_task_ID");
+  taskCountArea.textContent = `${taskCount} zadań`;
 }
